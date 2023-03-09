7ec5e8f511b9a1a03d19b054d1b2dc8099c0b245
Brian Vo <brianhvo02@gmail.com>
2023-03-08 16:41:46 -0800

    Completed project

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -17,8 +17,34 @@ class UsersController < ApplicationController
     end 
 
     def show
-        render json: params 
+        # render json: params
+        user = User.find_by(id: params[:id])
+        if (user)
+            render json: user
+        else
+            render plain: 'user not found', status: :not_found
+        end
     end 
+
+    def update
+        user = User.find_by(id: params[:id])
+        user.update(strong_params)
+
+        if user
+            render json: user
+        else
+            render plain: 'user not found', status: :not_found
+        end
+    end
+
+    def destroy
+        destroyed_user = User.destroy_by(id: params[:id]).first
+        if destroyed_user
+            render json: destroyed_user
+        else
+            render plain: 'user not found', status: :not_found
+        end
+    end
 
     private 
     def strong_params
--- first_routes_and_controllers/db/seeds.rb
+++ first_routes_and_controllers/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "Vamshi", email: "vamshi@gmail.com");
+User.create(name: "Brian", email: "brian@gmail.com");
+User.create(name: "Darren", email: "darren@gmail.com");
